b04ec0a4686a534393fbd0f17f5e7120d993a665
Daniel Perez Alvarez <unindented@gmail.com>
2016-12-09 19:23:53 +0000

    Remove @types/jasmine from typings

--- index.d.ts
+++ index.d.ts
@@ -1,28 +1,22 @@
-/// <reference types="jasmine"/>
-
-declare namespace jasmine {
-    interface Matchers {
-        toExist(): boolean;
-        toHaveLength(len: number): boolean;
-        toHaveId(id: string): boolean;
-        toHaveClass(klass: string): boolean;
-        toHaveTag(tag: string): boolean;
-        toHaveAttr(key: string, val?: any): boolean;
-        toHaveProp(key: string, val?: any): boolean;
-        toHaveText(text: string | RegExp): boolean;
-        toHaveData(key: string, val?: any): boolean;
-        toHaveValue(val: any): boolean;
-        toHaveCss(css: {[key: string]: any}): boolean;
-        toBeChecked(): boolean;
-        toBeDisabled(): boolean;
-        toBeEmpty(): boolean;
-        toBeHidden(): boolean;
-        toBeSelected(): boolean;
-        toBeVisible(): boolean;
-        toBeFocused(): boolean;
-        toBeInDom(): boolean;
-        toBeMatchedBy(sel: string): boolean;
-        toHaveDescendant(sel: string): boolean;
-        toHaveDescendantWithText(sel: string, text: string | RegExp): boolean;
-    }
-}
+export function toExist(): boolean
+export function toHaveLength(len: number): boolean
+export function toHaveId(id: string): boolean
+export function toHaveClass(klass: string): boolean
+export function toHaveTag(tag: string): boolean
+export function toHaveAttr(key: string, val?: any): boolean
+export function toHaveProp(key: string, val?: any): boolean
+export function toHaveText(text: string | RegExp): boolean
+export function toHaveData(key: string, val?: any): boolean
+export function toHaveValue(val: any): boolean
+export function toHaveCss(css: {[key: string]: any}): boolean
+export function toBeChecked(): boolean
+export function toBeDisabled(): boolean
+export function toBeEmpty(): boolean
+export function toBeHidden(): boolean
+export function toBeSelected(): boolean
+export function toBeVisible(): boolean
+export function toBeFocused(): boolean
+export function toBeInDom(): boolean
+export function toBeMatchedBy(sel: string): boolean
+export function toHaveDescendant(sel: string): boolean
+export function toHaveDescendantWithText(sel: string, text: string | RegExp): boolean
